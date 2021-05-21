839df372ff38ad14bfe751d4207ef1db5e5851ed
Eanew <H.Eanew@yandex.ru>
2021-05-21 19:49:20 +0300

    Добавляет файл с заданием

---
+++ stack.js
@@ -0,0 +1,63 @@
+const MAX_STACK_SIZE_BY_DEFAULT = 10
+
+const isValidMaxSize = number => Boolean(
+    typeof number === `number` &&
+    !isNaN(number) &&
+    number > 0 &&
+    number < Infinity
+)
+
+const Stack = class {
+    constructor(maxSize = MAX_STACK_SIZE_BY_DEFAULT) {
+        if (!isValidMaxSize(maxSize)) throw new Error(`Указан некорректный максимальный размер для стека`)
+
+        this._container = {}
+        this._maxSize = maxSize
+        this._size = 0
+    }
+    
+    isEmpty = () => !this._size
+    
+    push(element) {
+        if (this._size === this._maxSize) throw new Error(`Невозможно добавить элемент: стек переполнен`)
+
+        this._container[this._size++] = element
+        return this
+    }
+    
+    pop() {
+        if (this.isEmpty()) throw new Error(`Невозможно удалить элемент: стек пуст`)
+
+        const topElement = this._container[this._size - 1]
+
+        delete this._container[--this._size]
+        return topElement
+    }
+
+    peek = () => this.isEmpty() ? null : this._container[this._size - 1]
+    
+    toArray() {
+        const result = new Array(this._size)
+    
+        for (let i = 0, top = this._size - 1; i < result.length;) result[i++] = this._container[top--]
+        return result
+    }
+
+    static fromIterable(iterable) {
+        const stack = new Stack([...iterable].length)
+
+        for (const element of iterable) stack._container[stack._size++] = element
+        return stack
+    }
+    
+    // toArray() {
+    //     const result = []
+
+    //     while (!this.isEmpty()) result.push(this.pop())
+
+    //     for (let i = 0; i < result.length; i++) this.push(result[i])
+    //     return result
+    // }
+}
+
+module.exports = { Stack }
